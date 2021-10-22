2268383b7c489f2cf775d261a70869af3d9a98f4
cokelid <13600966+cokelid@users.noreply.github.com>
2021-10-22 17:56:26 +0100

    Minor change to logging message

--- package/ChocoButler/tools/chocolateybeforemodify.ps1
+++ package/ChocoButler/tools/chocolateybeforemodify.ps1
@@ -6,7 +6,7 @@ $pid_file ="$ENV:LOCALAPPDATA\ChocoButler.pid"
 
 if (Test-Path $pid_file) {
     $pid_from_file = Get-Content -Path $pid_file
-    Write-Host "[$((Get-Date).toString())] Killing existing ChocoButler session prior to update. PID = $pid_from_file [$pid_file]"
+    Write-Host "[$((Get-Date).toString())] Killing existing ChocoButler session prior to update or uninstall. PID = $pid_from_file [$pid_file]"
     Stop-Process -ID $pid_from_file
     Remove-Item -Path $pid_file
 } Else {
