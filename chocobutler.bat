b6fd0f241c004fd3ae13e080c6fa43f1c18da00b
cokelid <13600966+cokelid@users.noreply.github.com>
2023-06-08 11:45:54 +0100

    Add note to .bat file on problems with hidden window

--- chocobutler.bat
+++ chocobutler.bat
@@ -3,4 +3,9 @@
 @ECHO OFF
 SET ThisDir=%~dp0
 SET ScriptPath=%ThisDir%chocobutler.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -windowstyle hidden -Command "& '%ScriptPath%'";
+:: The following fails to hide the window in Windows 11, see: https://github.com/microsoft/terminal/issues/12464
+:: Can we start the powershell script without using a .bat file? Chocobutler has a wrapper for .bat to .exe?
+:: See: https://stackoverflow.com/questions/69259601/shim-a-bat-file-in-chocolatey
+:: The shim functionality: https://docs.chocolatey.org/en-us/create/functions/install-binfile
+:: Maybe try to convert the .ps1 to and .exe with https://github.com/MScholtes/PS2EXE
+PowerShell -NoProfile -ExecutionPolicy Bypass -WindowStyle hidden -Command "& '%ScriptPath%'";
